83014149776bce403396659bbb6241ecae2c6064
oneforaru <oneforaru@1ef8504e-d577-11dd-ba88-affbdc068414>
2009-01-06 22:25:10 +0000

    Fixing a bug with textures that have a name of less than 6 chars.

--- RageLib/Textures/Texture.cs
+++ RageLib/Textures/Texture.cs
@@ -138,7 +138,7 @@ namespace RageLib.Textures
 
         public override string ToString()
         {
-            return string.Format("{0} ({1}x{2} {3})", Name.Substring(6), Width, Height, TextureType);
+            return string.Format("{0} ({1}x{2} {3})", Name, Width, Height, TextureType);
         }
 
         public uint GetTextureDataSize(int level)
